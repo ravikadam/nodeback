2aa6cb50fcd83128e144ab24b283546e400b7162
Ravikadam <ravindrakad@gmail.com>
2020-03-01 18:33:40 +0530

    Compiled error free

--- src/interfaces/http/auth.js
+++ src/interfaces/http/auth.js
@@ -2,12 +2,7 @@ import passport from 'passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth'
 import FacebookStrategy from 'passport-facebook'
-import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2'
-import InstagramStrategy from 'passport-instagram'
-import TwitterStrategy from 'passport-twitter-oauth2'
-import { Strategy as MicrosoftStrategy } from 'passport-microsoft'
-import AmazonStrategy from 'passport-amazon'
-import GithubStrategy from 'passport-github'
+
 
 /**
  * middleware to check the if auth vaid
@@ -73,210 +68,7 @@ module.exports = ({
     }
   )
 
-  const twitterStrategy = new TwitterStrategy(
-    {
-      clientID: process.env.TWITTER_APP_ID,
-      clientSecret: process.env.TWITTER_APP_SECRET,
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.TWITTER_CALLBACK_PATH
-      }`
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'twitter', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName,
-          familyName: profile.name ? profile.name.familyName : null,
-          parentImage:
-            profile.photos && profile.photos.length > 0
-              ? profile.photos[0].value
-              : null,
-          email: profile.email,
-          locale: profile.locale,
-          emailVerified: profile.email ? true : false,
-          isActive: true,
-          loginType: 'twitter'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
-  const microsoftStrategy = new MicrosoftStrategy(
-    {
-      clientID: process.env.MICROSOFT_APP_ID,
-      clientSecret: process.env.MICROSOFT_APP_SECRET,
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.MICROSOFT_CALLBACK_PATH
-      }`,
-      scope: ['https://graph.microsoft.com/.default']
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'microsoft', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName,
-          familyName: profile.name ? profile.name.familyName : null,
-          parentImage:
-            profile.photos && profile.photos.length > 0
-              ? profile.photos[0].value
-              : null,
-          email: profile.email,
-          locale: profile.locale,
-          emailVerified: profile.email ? true : false,
-          isActive: true,
-          loginType: 'microsoft'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
-  const amazonStrategy = new AmazonStrategy(
-    {
-      clientID: process.env.AMAZON_APP_ID,
-      clientSecret: process.env.AMAZON_APP_SECRET,
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.AMAZON_CALLBACK_PATH
-      }`
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'amazon', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName,
-          familyName: profile.name ? profile.name.familyName : null,
-          parentImage:
-            profile.photos && profile.photos.length > 0
-              ? profile.photos[0].value
-              : null,
-          email: profile.email,
-          locale: profile.locale,
-          emailVerified: profile.email ? true : false,
-          isActive: true,
-          loginType: 'amazon'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
-  const githubStrategy = new GithubStrategy(
-    {
-      clientID: process.env.GITHUB_APP_ID,
-      clientSecret: process.env.GITHUB_APP_SECRET,
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.GITHUB_CALLBACK_PATH
-      }`
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'github', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName
-            ? profile.displayName
-            : profile.username,
-          parentImage:
-            profile.photos && profile.photos.length > 0
-              ? profile.photos[0].value
-              : null,
-          isActive: true,
-          loginType: 'github'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
-  const instagramStrategy = new InstagramStrategy(
-    {
-      clientID: process.env.INSTAGRAM_APP_ID,
-      clientSecret: process.env.INSTAGRAM_APP_SECRET,
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.INSTAGRAM_CALLBACK_PATH
-      }`
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'instagram', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName,
-          familyName: profile.name ? profile.name.familyName : null,
-          parentImage:
-            profile._json &&
-            profile._json.data &&
-            profile._json.data.profile_picture
-              ? profile._json.data.profile_picture
-              : null,
-          isActive: true,
-          loginType: 'instagram'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
-  const linkedInStrategy = new LinkedInStrategy(
-    {
-      clientID: process.env.LINKEDIN_APP_ID,
-      clientSecret: process.env.LINKEDIN_APP_SECRET,
-      scope: ['r_emailaddress', 'r_liteprofile'],
-      callbackURL: `${process.env.APP_BASE_URL}${
-        process.env.LINKEDIN_CALLBACK_PATH
-      }`
-    },
-    async (token, refreshToken, profile, done) => {
-      await createUserIfNotExists(profile.id, 'linkedin', {
-        body: {
-          socialId: profile.id,
-          parentName: profile.displayName,
-          familyName: profile.name ? profile.name.familyName : null,
-          parentImage:
-            profile.photos && profile.photos.length > 0
-              ? profile.photos[0].value
-              : null,
-          email:
-            profile.emails && profile.emails.length > 0
-              ? profile.emails[0].value
-              : null,
-          emailVerified:
-            profile.emails &&
-            profile.emails.length > 0 &&
-            profile.emails[0].value
-              ? true
-              : false,
-          isActive: true,
-          loginType: 'linkedin'
-        }
-      })
-
-      return done(null, {
-        profile: profile,
-        token: token
-      })
-    }
-  )
-
+  
   const googleStrategy = new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
@@ -310,12 +102,7 @@ module.exports = ({
   passport.use(strategy)
   passport.use(googleStrategy)
   passport.use(facebookStrategy)
-  passport.use(linkedInStrategy)
-  passport.use(instagramStrategy)
-  passport.use(twitterStrategy)
-  passport.use(microsoftStrategy)
-  passport.use(amazonStrategy)
-  passport.use(githubStrategy)
+  
 
   passport.serializeUser(function(user, done) {
     done(null, user)
